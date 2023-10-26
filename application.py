832c7fe17b5b5b47fc0cfd3d3441b6824789a95f
­양동선(엘텍공과대학 소프트웨어학부) <ydsperfect08@ewhain.net>
2023-10-26 09:24:56 +0000

    df

--- application.py
+++ application.py
@@ -23,27 +23,29 @@ def reg_item():
 def reg_review():
     return render_template("reg_reviews.html")
 
-if __name__=="__main__":
-    application.run(host='0.0.0.0', debug=True)
-    
 @application.route("/submit_item")
 def reg_item_submit():
-    name=request.args.get("name") 
-    seller=request.args.get("seller") 
-    addr=request.args.get("addr") 
-    email=request.args.get("email") 
-    category=request.args.get("category") 
-    card=request.args.get("card") 
-    status=request.args.get("status") 
-    phone=request.args.get("phone")
+    name = request.args.get("name")
+    seller = request.args.get("seller")
+    addr = request.args.get("addr")
+    email = request.args.get("email")
+    category = request.args.get("category")
+    card = request.args.get("card")
+    status = request.args.get("status")
+    phone = request.args.get("phone")
 
-    print(name,seller,addr,email,category,card,status,phone)
+    print(name,addr,tel,category,park,time,site)
     #return render_template("reg_item.html")
+    
 @application.route("/submit_item_post", methods=['POST']) 
 def reg_item_submit_post():
-    
-    image_file=request.files["file"] 
+    print("11111111")
+    image_file = request.files["file"]
     image_file.save("static/images/{}".format(image_file.filename))
-    data=request.form
-    return render_template("submit_item_result.html", data=data,
-                           img_path="static/images/{}".format(image_file.filename)                   )
+    data = request.form
+    print("2222")
+    return render_template("submit_item_result.html", data=data, img_path="static/images/{}".format(image_file.filename))
+
+if __name__== "__main__":
+    application.run(host='0.0.0.0', debug=True)
+
